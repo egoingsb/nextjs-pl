31347d1ea012c2ae3377d7a95391c40f03b86eec
going lee <egoing@gmail.com>
2023-01-27 18:44:55 +0900

    form

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,12 @@
 import { Header } from "./Header";
 import { Nav } from "./Nav";
+import Link from 'next/link';
 
 export default function Layout({children}){
   return <>
     <Header title="Payletter"></Header>
     <Nav></Nav>
     {children}
+    <Link href="/create">Create</Link>
   </>
 }
