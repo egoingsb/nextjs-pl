40e3a446d725915816efaae43e2d00153fd05498
going lee <egoing@gmail.com>
2023-01-27 19:31:22 +0900

    contextUI

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,21 @@
 import { Header } from "./Header";
 import { Nav } from "./Nav";
 import Link from 'next/link';
+import { useRouter } from "next/router";
 
 export default function Layout({children}){
+  const router = useRouter();
+  let contextUI = null;
+  if(router.query.id){
+    contextUI = <>
+      <Link href={`/update/${router.query.id}`}>Update</Link>
+    </>
+  }
   return <>
     <Header title="Payletter"></Header>
     <Nav></Nav>
     {children}
     <Link href="/create">Create</Link>
+    {contextUI}
   </>
 }
