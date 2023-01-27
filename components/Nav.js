340bab398cf2bcfd72d6811b304f74d1a3169eb4
going lee <egoing@gmail.com>
2023-01-27 17:44:18 +0900

    fetch

--- components/Nav.js
+++ components/Nav.js
@@ -1,10 +1,16 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export function Nav() {
-  const topics = [
-    { id: 1, title: "About", body: "About Payletter" },
-    { id: 2, title: "History", body: "History of Payletter" },
-  ];
+  const [topics, setTopics] = useState([]);
+  async function init(){
+    const resp = await fetch('http://localhost:9999/topics');
+    const result = await resp.json();
+    setTopics(result);
+  }
+  useEffect(()=>{
+    init();
+  },[]);
   const lis = topics.map((e) => <li key={e.id}>
     <Link href={`/read/${e.id}`}>{e.title}</Link>
   </li>);
