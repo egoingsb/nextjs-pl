e704736e5c6b81381c85866049b4d15505702ca3
going lee <egoing@gmail.com>
2023-01-30 10:47:59 +0900

    grid

--- components/Layout.js
+++ components/Layout.js
@@ -2,6 +2,7 @@ import { Header } from "./Header";
 import { Nav } from "./Nav";
 import Link from 'next/link';
 import { useRouter } from "next/router";
+import { Container, Grid } from "@mui/material";
 
 export default function Layout({children}){
   const router = useRouter();
@@ -11,11 +12,17 @@ export default function Layout({children}){
       <Link href={`/update/${router.query.id}`}>Update</Link>
     </>
   }
-  return <>
+  return <Container maxWidth="md">
     <Header title="Payletter"></Header>
-    <Nav></Nav>
-    {children}
-    <Link href="/create">Create</Link>
-    {contextUI}
-  </>
+    <Grid container>
+      <Grid item md={3} xs={12}>
+        <Nav></Nav>
+      </Grid>
+      <Grid item md={9} xs={12}>
+        {children}
+        <Link href="/create">Create</Link>
+        {contextUI}
+      </Grid>
+    </Grid>
+  </Container>
 }
